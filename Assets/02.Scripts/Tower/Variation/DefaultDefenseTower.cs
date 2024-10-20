780b9e30395e6d3d0c8ca5e856fdb8f28a685b5f
lend-a-villin <lend_a_villin@naver.com>
2024-10-20 21:20:30 +0900

    디펜스 타워로 경로 차단해도 몬스터 정상작동하게 수정.

--- Assets/02.Scripts/Tower/Variation/DefaultDefenseTower.cs
+++ Assets/02.Scripts/Tower/Variation/DefaultDefenseTower.cs
@@ -1,4 +1,5 @@
 using FoxHill.Core;
+using FoxHill.Core.Pause;
 using FoxHill.Core.Utils;
 using FoxHill.Monster;
 using ProjectDawn.Navigation;
@@ -14,81 +15,72 @@ namespace FoxHill.Tower
     public class DefaultDefenseTower : DefenseTowerControllerBase
     {
         [SerializeField] private float getDamageInterval = 1f;
-        private CrowdObstacleAuthoring _crowdObstacle;
-        private Vector2[] transforms;
-        private HashSet<Collision2D> _monsterSet = new HashSet<Collision2D>(256);
+        private HashSet<Collider2D> _monsterSet = new HashSet<Collider2D>(256);
         private HashSet<Transform> _monsterTransformSet = new HashSet<Transform>(256);
         private Coroutine _periodlyGetDamageCoroutine;
         private float _intervalStartTime;
         private float _elapsedTime;
 
-        // todo.Villin 타워 어떻게 해야 몬스터가 타워에 비비게 할 수 있을지.
         protected override void Awake()
         {
             base.Awake();
-            _crowdObstacle = GetComponent<CrowdObstacleAuthoring>();
-            _crowdObstacle.enabled = false;
+            PauseManager.Register(this);
         }
 
         protected override IEnumerator PerformTowerFunction()
         {
             yield return null;
         }
-        private void OnCollisionEnter2D(Collision2D collision)
+        private void OnTriggerEnter2D(Collider2D collider)
         {
-            if (collision.gameObject.layer == LayerRepository.LAYER_PATH_FOLLOW_MONSTER && !_monsterSet.Contains(collision))
+            if (collider.gameObject.layer == LayerRepository.LAYER_PATH_FOLLOW_MONSTER && !_monsterSet.Contains(collider))
             {
-                _monsterSet.Add(collision);
-                float power = collision.gameObject.GetComponent<PathFollowMonsterController>().Power;
-                if (_crowdObstacle.enabled == false)
+                _monsterSet.Add(collider);
+                float power = collider.gameObject.GetComponent<PathFollowMonsterController>().Power;
+                AgentAuthoring agentAuthoring = collider.gameObject.GetComponent<AgentAuthoring>();
+                if (agentAuthoring.enabled == true)
                 {
-                    _crowdObstacle.enabled = true;
+                    agentAuthoring.enabled = false;
                 }
 
                 if (_periodlyGetDamageCoroutine == null)
                 {
-                    _periodlyGetDamageCoroutine = StartCoroutine(PeriodlyGetDamage(power, _monsterTransformSet));
+                    _periodlyGetDamageCoroutine = StartCoroutine(PeriodlyGetDamage(power, _monsterSet));
                 }
             }
         }
 
-        private void OnCollisionExit2D(Collision2D collision)
+        private void OnTriggerExit2D(Collider2D collider)
         {
-            if (collision.gameObject.layer == LayerRepository.LAYER_PATH_FOLLOW_MONSTER && !_monsterSet.Contains(collision))
+            if (collider.gameObject.layer == LayerRepository.LAYER_PATH_FOLLOW_MONSTER)
             {
-                _monsterSet.Remove(collision);
+                AgentAuthoring agentAuthoring = collider.gameObject.GetComponent<AgentAuthoring>();
+                if (agentAuthoring.enabled == false)
+                {
+                    agentAuthoring.enabled = true;
+                }
+                _monsterSet.Remove(collider);
                 if (_monsterSet.Count == 0 && _periodlyGetDamageCoroutine != null)
                 {
-                    if (_crowdObstacle.enabled == false)
-                    {
-                        _crowdObstacle.enabled = true;
-                    }
                     StopCoroutine(_periodlyGetDamageCoroutine);
                     _periodlyGetDamageCoroutine = null;
                 }
             }
         }
 
-        private IEnumerator PeriodlyGetDamage(float damage, HashSet<Transform> transformSet)
+        private IEnumerator PeriodlyGetDamage(float damage, HashSet<Collider2D> monsterSet)
         {
             Vector2 position = transform.position;
             DebugFox.Log("PeriodlyGetDamage Coroutine Started");
             if (_intervalStartTime == default)
             { _intervalStartTime = Time.time; }
-            foreach (var transform in transformSet)
+            foreach (var monster in monsterSet)
             {
                 int i = 0;
-                transforms[i] = transform.transform.position;
                 i++;
             }
             while (_monsterSet.Count > 0)
             {
-                foreach (var transform in transformSet)
-                {
-                    int i = 0;
-                    transform.position = transforms[i];
-                        i++;
-                }
                 if (_isPaused == true)
                 {
                     float pauseStartedTime = Time.time;
@@ -103,6 +95,15 @@ namespace FoxHill.Tower
                     _stat.CurrentHp -= totalDamage;
                     if (_stat.CurrentHp <= 0f)
                     {
+                        foreach (var monster in _monsterSet)
+                        {
+                            AgentAuthoring agentAuthoring = monster.gameObject.GetComponent<AgentAuthoring>();
+
+                            if (agentAuthoring.enabled == false)
+                            {
+                                agentAuthoring.enabled = true;
+                            }
+                        }
                         Destroy(gameObject);
                     }
                     _elapsedTime = 0f;
